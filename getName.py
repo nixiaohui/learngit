abd7e958b2f554a9d7ff160e05bc09b5b1600625
nixiaohui <6089870@qq.com>
2020-06-01 18:05:20 +0800

    增加python文件

---
+++ getName.py
@@ -0,0 +1,2 @@
+def get_name():
+    return input("请输入您的名字：")
--- sayhello.py
+++ sayhello.py
@@ -1,5 +1,9 @@
+from getName import get_name
+
 def say_hello(name="NoBody"):
     print("Hi,", name)
     
 if __name__ == "__main__":
+    name = get_name()
     say_hello("hugo")
+    say_hello(name)
